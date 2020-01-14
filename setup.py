329cc56a9e4fd90322e5809b4396de9a5e7d4580
Frode Hus <frode.hus@outlook.com>
2020-01-14 20:37:12 +0100

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(dir, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="kuberesources",
-    version="1.4.1",
+    version="1.5",
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
@@ -24,6 +24,7 @@ setuptools.setup(
     install_requires=[
         "kubernetes>=11.0.0b2",
         "pyinquirer",
+        "prettytable",
         "adal",
         "colorama",
     ],
